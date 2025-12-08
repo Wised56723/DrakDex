e2358373c7634283a66214f711f1de0316d75166
Wised56723 <luis200.lh98@gmail.com>
2025-12-08 10:33:40 -0300

    fix: configurando CORS e APIs para deploy

--- src/main/java/com/luis/drakdex/config/OpenApiConfig.java
+++ src/main/java/com/luis/drakdex/config/OpenApiConfig.java
@@ -1,23 +1,35 @@
 package com.luis.drakdex.config;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration; // <--- Importante
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.info.License; // <--- Importante
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
+
+    // Lê a URL do Railway das variáveis (ou usa localhost se não existir)
+    @Value("${railway.public.url:http://localhost:8080}")
+    private String railwayUrl;
 
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
             .info(new Info()
-                .title("DrakDex API") // O Título do teu projeto
+                .title("DrakDex API")
                 .version("1.0")
                 .description("Documentação oficial do Bestiário de RPG DrakDex")
                 .termsOfService("http://swagger.io/terms/")
-                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+                .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+            .servers(List.of(
+                new Server().url(railwayUrl).description("Servidor Principal"),
+                new Server().url("http://localhost:8080").description("Servidor Local")
+            ));
     }
 }
