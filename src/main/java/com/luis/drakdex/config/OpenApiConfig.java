bb6381fc696eaddb6dbb3769ac42231947b3c7e2
Wised56723 <luis200.lh98@gmail.com>
2025-12-11 23:41:01 -0300

    fix(security): autorização com bearer token para swagger

--- src/main/java/com/luis/drakdex/config/OpenApiConfig.java
+++ src/main/java/com/luis/drakdex/config/OpenApiConfig.java
@@ -4,19 +4,21 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration; // <--- Importante
+import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License; // <--- Importante
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class OpenApiConfig {
 
-    // Lê a URL do Render das variáveis (ou usa localhost se não existir)
-    @Value("${https://drakdex-api.onrender.com:http://localhost:8080}")
-    private String ServerURL;
+    @Value("${SERVER_URL:http://localhost:8080}")
+    private String serverUrl;
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -25,11 +27,21 @@ public class OpenApiConfig {
                 .title("DrakDex API")
                 .version("1.0")
                 .description("Documentação oficial do Bestiário de RPG DrakDex")
-                .termsOfService("http://swagger.io/terms/")
                 .license(new License().name("Apache 2.0").url("http://springdoc.org")))
             .servers(List.of(
-                new Server().url("https://drakdex-api.onrender.com").description("Servidor de Produção (Render)"),
+                // Garante que o teu link do Render está aqui
+                new Server().url("https://drakdex-api.onrender.com").description("Servidor de Produção"),
                 new Server().url("http://localhost:8080").description("Servidor Local")
-            ));
+            ))
+            // --- A MÁGICA ACONTECE AQUI ---
+            // 1. Dizemos que toda a API precisa de segurança
+            .addSecurityItem(new SecurityRequirement().addList("bearer-key"))
+            // 2. Definimos como é essa segurança (Token JWT)
+            .components(new Components()
+                .addSecuritySchemes("bearer-key",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")));
     }
 }
